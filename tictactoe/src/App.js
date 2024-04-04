3de83c7bb3db489161e5d674188be62382a8ce9c
TAZ-Creative <olawaleazeem6@gmail.com>
2024-04-04 11:06:59 +0100

    Clearing errors

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,15 +1,49 @@
-export default function Square() {
+function Square () {
+  return  <button className="square">X1</button>;
+}
+
+export default function Board () {
   return (
-  <>
-  <button className="square">X</button>
-  <button className="square">X</button>
-  <button className="square">X</button>
-  <button className="square">X</button>
-  <button className="square">X</button>
-  <button className="square">X</button>
-  <button className="square">X</button>
-  <button className="square">X</button>
-  <button className="square">X</button>
-  </>
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   );
 }
+   
+// export default function Board() {
+//   return (
+//   <>
+//     <div className="board-row">
+//       <button className="square">X1</button>
+//       <button className="square">X2</button>
+//       <button className="square">X3</button>
+//     </div>
+//     <div className="board-row">
+//       <button className="square">X4</button>
+//       <button className="square">X5</button>
+//       <button className="square">X6</button>
+//     </div>
+//     <div className="board-row">
+//       <button className="square">X7</button>
+//       <button className="square">X8</button>
+//       <button className="square">X9</button>
+//     </div>
+//   </>
+//   );
+// }
+
+

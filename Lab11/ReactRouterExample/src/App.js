026524b2be2170f556c1698b7e7018cd6f480e5d
TAZ-Creative <olawaleazeem6@gmail.com>
2024-05-16 16:39:17 +0100

    Organizing pages into the pages folder

--- Lab11/ReactRouterExample/src/App.js
+++ Lab11/ReactRouterExample/src/App.js
@@ -1,12 +1,10 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import {Home} from './components/Home'
-import {ProductList} from './components/ProductList'
-import { Contact } from './components/Contact'; // Import the Contact component
+import {ProductList, Contact, Admin, ProductDetail, NotFound, Home} from './pages'
 import './App.css';
 import { Header } from './components/Header';
-import { Admin } from './components/Admin';
-import { ProductDetail } from './components/ProductDetail';
-import { NotFound } from './components/Notfound';
+import { Footer } from './components/Footer';
+
+
 
 function App() {
 
@@ -25,7 +23,7 @@ function App() {
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
 
-      <footer>FOOTER</footer>  
+      <Footer/>  
     </div>
     </BrowserRouter>
   );
